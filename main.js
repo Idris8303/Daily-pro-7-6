f238e7f384b35ce4cc5859a08b041df1a8a3ae78
Idris8303 <bctxtoast@gmail.com>
2017-07-06 17:11:53 -0500

    pushing daily pro

---
+++ main.js
@@ -0,0 +1,106 @@
+// 1.
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// Then, write and example of using the function.
+
+function max(){
+    //
+}
+
+
+// 2.
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// Then, write and example of using the function.
+
+function maxOfThree(){
+    // Your answer here
+}
+
+
+// 3.
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// Then, write and example of using the function.
+
+function isVowel(char){
+    // Your answer here
+}
+
+
+// 4.
+// Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
+// Then, write and example of using the function.
+
+
+
+// 5.
+// Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
+// Then, write and example of using the function.
+function avg(num1, num2, num3) {
+  return num1 + num2 + num3 / 3;
+}
+let sumNumbers = avg(2, 4, 8);
+console.log(avg(2, 4, 8) === 4.6666);
+
+
+
+
+
+
+// 6.
+// Write a function called `getLength` that takes one parameter (a string) and returns the length
+// Then, write and example of using the function.
+function getLength(add){
+  if ('add'){
+  }
+    return 'Length';
+}
+console.log(getLength('add'));
+
+
+
+
+
+
+// 7.
+// Write a function called `greaterThan` that takes two parameters
+// and returns `true` if the second parameter is greater than the first.
+// Otherwise the function should return `false`.
+// Then, write and example of using the function.
+function greaterThan (num1, num2) {
+  if(num1 > num2) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+}
+console.log(greaterThan(6, 4) === true);
+
+
+
+// 8.
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+// Then, write and example of using the function.
+function greet (Hello) {
+  return 'Hello Jake!';
+  }
+  console.log(greet('Hello Jake!'));
+
+
+
+
+
+
+
+
+// 9.
+// Write a function called `madlib` that takes 4 or more parameters (words).
+// The function should insert the words into a pre-defined sentence.
+// Finally the function should return that sentence.
+// Note: When I say words and sentence I mean strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+// Then, write and example of using the function.
